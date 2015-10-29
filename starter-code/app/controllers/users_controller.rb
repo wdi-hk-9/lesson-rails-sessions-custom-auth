e58c1d67a1d044359d920a2eaa86d55fee920714
Fer Martin <fernandomartinsantana@gmail.com>
2015-10-29 16:19:12 +0800

    User has name

--- starter-code/app/controllers/users_controller.rb
+++ starter-code/app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
  end
 
  def user_params
-    params.require(:user).permit( :email, :password, :password_confirmation)
+    params.require(:user).permit( :email, :password, :password_confirmation, :name)
   end
 
 end
--- starter-code/db/schema.rb
+++ starter-code/db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150707132754) do
+ActiveRecord::Schema.define(version: 20151029075920) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "name"
   end
 
 end
